12d5c284a79ded402391d9410b67b1bcf263459c
lol-russo <jack.lorusso@digivizer.com>
2018-01-22 23:00:58 +1100

    Change <blockquote> to <p>

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <blockquote>{randomDescription(quotes)}</blockquote>
+        <p>{randomDescription(quotes)}</p>
       </div>
     );
   }
